2ad5c91e05e0c527998b77fd91abaab05a471572
Amanda Kom <mandareneekom@gmail.com>
2015-03-23 10:53:10 -0700

    Finished Add a DVD Test feature

--- test/features/dvds/add_a_dvd.rb
+++ test/features/dvds/add_a_dvd.rb
@@ -1,6 +1,31 @@
 require "test_helper"
 
 feature "add a DVD" do
-  scenario "test" do
+  scenario "a new DVD path exists" do
+    visit dvds_path
+    click_on "Request a new DVD"
+    page.must_have_content "New DVD Request"
+  end
+
+  scenario "user can request new DVDs" do
+    visit dvds_path
+    click_on "Request a new DVD"
+    fill_in "Title", with: "Divergent"
+    fill_in "Genre", with: "Adventure, Sci-Fi, Thriller"
+    fill_in "Year", with: 2014
+    select('PG-13', from: 'Rating')
+    click_on "Submit request"
+    page.must_have_content "Thank you for your request."
+  end
+
+  scenario "an admin can approve a new DVD request" do
+    visit dvds_path
+    click_on "Request a new DVD"
+    fill_in "Title", with: "The Princess Bride"
+    fill_in "Genre", with: "Adventure, Comedy, Family"
+    fill_in "Year", with: 1987
+    select('PG', from: 'Rating')
+    click_on "Approved"
+    page.must_have_content "The Princess Bride has been added."
   end
 end
