66afee465c93b75549ec7831fdf80d269344a7ce
David Ker <snappy316@gmail.com>
2015-03-23 15:37:21 -0700

    Add an address field to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150323190305) do
+ActiveRecord::Schema.define(version: 20150323223633) do
 
   create_table "dvds", force: :cascade do |t|
     t.string   "genre"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20150323190305) do
     t.datetime "oauth_expires_at"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+    t.text     "address"
   end
 
 end
