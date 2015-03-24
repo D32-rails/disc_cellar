5c83855acf72ee76c70fca7516846d1b44456310
David Ker <snappy316@gmail.com>
2015-03-24 08:55:35 -0700

    Fix show user test to not puts page.text

--- test/features/user/show_user_test.rb
+++ test/features/user/show_user_test.rb
@@ -3,7 +3,6 @@ require "test_helper"
 feature "Show a user" do
   scenario "can see an individual user profile" do
     visit users_path
-    puts page.text
     page.must_have_content("Members")
   end
 end
