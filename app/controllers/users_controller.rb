775cf68070c67fe99410286ef2f0d591992d9429
David Ker <snappy316@gmail.com>
2015-03-24 13:44:04 -0700

    Update the user model and controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,30 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
   def index
     @user = User.all
   end
 
   def edit
-    set_user
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user, notice: "Thank you for your request."
+    else
+      render :edit
+    end
   end
 
   def show
-    set_user
   end
 
   private
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:address, :role)
   end
 end
