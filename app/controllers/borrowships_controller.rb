bfbe77f3521281da54156ccffea8a615c39ccae5
David Ker <snappy316@gmail.com>
2015-03-25 14:54:17 -0700

    Fix borrowship controller, and add a "Ask to Borrow" button on user show

--- app/controllers/borrowships_controller.rb
+++ app/controllers/borrowships_controller.rb
@@ -1,12 +1,23 @@
 class BorrowshipsController < ApplicationController
   def create
     @borrowship = current_user.borrowships.build(borrowship_params)
-    if @borrowship.save
-      flash[:notice] = "Request sent to the lender!"
-      redirect_to root_url
+    # if @borrowship.save
+    #   flash[:notice] = "Request sent to the lender!"
+    #   redirect_to dvds_path
+    # else
+    #   flash[:notice] = "Unable to add request."
+    #   redirect_to dvds_path
+    # end
+  end
+
+  def update
+    @borrowship = Borrowship.find(params[:id])
+    if @borrowship.update(borrowship_params)
+      flash[:notice] = "Request sent to lender!"
+      redirect_to dvds_path
     else
-      flash[:notice] = "Unable to add request."
-      redirect_to root_url
+      flash[:notice] = "Unable to request the DVD"
+      redirect_to dvds_path
     end
   end
 
